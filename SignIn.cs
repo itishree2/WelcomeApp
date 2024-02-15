a62ce7b238ff30200b34874cbc129fd30c4e1364
itishree2 <nayakitishree6262@gmail.com>
2024-02-15 12:05:53 +0530

    Final commit

---
+++ SignIn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    public class SignIn
+    {
+        public string Authenticate(string username, string password)
+        {
+            string msg;
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password)) 
+            {
+                msg = "User name and password required"; 
+                    
+             }  
+            else
+            {
+                if((username=="Mama") && (password=="Mama@123"))
+                {
+                    msg = "Authentication Pass";
+                }
+                else
+                {
+                    msg = "Authentication Fail";
+                }
+            }
+            return msg;
+        }
+    }
+}
